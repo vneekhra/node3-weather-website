3014edcd03cc98829e6cc9e2e31e6d46b1f0b8c7
vneekhra <vikasnikhra@gmail.com>
2020-01-19 21:14:12 -0700

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -23,9 +23,3 @@ weatherForm.addEventListener('submit', (e) => {
         })
     })
 })
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
