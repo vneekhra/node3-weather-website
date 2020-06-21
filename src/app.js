d2cbcc032e73cd289faa5174ab09347d8bd2f99d
vneekhra <vikasnikhra@gmail.com>
2020-06-20 23:02:04 -0600

    updated app.js file

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
+        title: 'Check Weather',
         name: 'learning...'
     })
 })
