f9f69fe631aab9b826fcdfcdc29fcba47b9dd40b
vneekhra <vikasnikhra@gmail.com>
2020-07-02 11:12:01 -0600

    blue green deployment

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Its green Weather!',
+        title: 'Its blue Weather again!!!!',
         name: 'learning...'
     })
 })
