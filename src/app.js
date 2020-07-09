25cab5e7e0513a2098cfa2f8438173d8d64d7cdd
vneekhra <vikasnikhra@gmail.com>
2020-07-08 22:15:49 -0600

    B side deployment

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather deployment A side',
+        title: 'Weather deployment B side',
         name: 'learning...'
     })
 })
