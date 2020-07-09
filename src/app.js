6387dc13c779764798e9052d794af10215a9c567
vneekhra <vikasnikhra@gmail.com>
2020-07-08 21:53:26 -0600

    ab deployment

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Its blue Weather again!!!!',
+        title: 'Weather deployment A side',
         name: 'learning...'
     })
 })
